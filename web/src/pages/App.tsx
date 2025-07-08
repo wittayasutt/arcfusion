22f1f184ce784a4d4a10bda4449a2c45c3b3b510
Ratthapon Wittayasuttikul <wittayasutt@gmail.com>
2025-07-08 20:42:55 +0700

    feature: setup toggle component

--- web/src/pages/App.tsx
+++ web/src/pages/App.tsx
@@ -1,5 +1,11 @@
+import { ToggleSidebar } from '@components';
+
 function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+		<>
+			<ToggleSidebar />
+		</>
+	);
 }
 
 export default App;
