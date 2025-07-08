72b082f2fe8982259f2e933c15cba58193f108e5
Ratthapon Wittayasuttikul <wittayasutt@gmail.com>
2025-07-08 17:55:05 +0700

    feature: setup basic

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(process.cwd(), 'src'),
+      '@components': path.resolve(process.cwd(), 'src/components'),
+      '@atoms': path.resolve(process.cwd(), 'src/components/atoms'),
+      '@molecules': path.resolve(process.cwd(), 'src/components/molecules'),
+      '@organisms': path.resolve(process.cwd(), 'src/components/organisms'),
+      '@templates': path.resolve(process.cwd(), 'src/components/templates'),
+      '@features': path.resolve(process.cwd(), 'src/features'),
+      '@pages': path.resolve(process.cwd(), 'src/pages'),
+      '@assets': path.resolve(process.cwd(), 'src/assets'),
+    },
+  },
 })
