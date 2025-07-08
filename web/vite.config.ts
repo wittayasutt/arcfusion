88c51e7c4925d32c8306f0a1cbe52cc5eeb768ce
Ratthapon Wittayasuttikul <wittayasutt@gmail.com>
2025-07-08 18:23:35 +0700

    feature: set up tailwind

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,21 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(process.cwd(), 'src'),
-      '@components': path.resolve(process.cwd(), 'src/components'),
-      '@atoms': path.resolve(process.cwd(), 'src/components/atoms'),
-      '@molecules': path.resolve(process.cwd(), 'src/components/molecules'),
-      '@organisms': path.resolve(process.cwd(), 'src/components/organisms'),
-      '@templates': path.resolve(process.cwd(), 'src/components/templates'),
-      '@features': path.resolve(process.cwd(), 'src/features'),
-      '@pages': path.resolve(process.cwd(), 'src/pages'),
-      '@assets': path.resolve(process.cwd(), 'src/assets'),
-    },
-  },
-})
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@': path.resolve(process.cwd(), 'src'),
+			'@components': path.resolve(process.cwd(), 'src/components'),
+			'@atoms': path.resolve(process.cwd(), 'src/components/atoms'),
+			'@molecules': path.resolve(process.cwd(), 'src/components/molecules'),
+			'@organisms': path.resolve(process.cwd(), 'src/components/organisms'),
+			'@templates': path.resolve(process.cwd(), 'src/components/templates'),
+			'@features': path.resolve(process.cwd(), 'src/features'),
+			'@pages': path.resolve(process.cwd(), 'src/pages'),
+			'@assets': path.resolve(process.cwd(), 'src/assets'),
+		},
+	},
+});
