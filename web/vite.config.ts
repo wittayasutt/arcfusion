abb0c342ff68b3f21f5ec7365a98a51a5e464677
Ratthapon Wittayasuttikul <wittayasutt@gmail.com>
2025-07-13 14:47:11 +0700

    feat: integrate Storybook into the project with necessary configurations and dependencies, including ESLint support and Vite test integration

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,18 @@
+/// <reference types="vitest/config" />
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+const dirname =
+	typeof __dirname !== 'undefined'
+		? __dirname
+		: path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	resolve: {
@@ -23,5 +32,33 @@ export default defineConfig({
 			'@types': path.resolve(__dirname, 'src/types'),
 			'@utils': path.resolve(__dirname, 'src/utils'),
 		},
+	},
+	test: {
+		projects: [
+			{
+				extends: true,
+				plugins: [
+					// The plugin will run tests for the stories defined in your Storybook config
+					// See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+					storybookTest({
+						configDir: path.join(dirname, '.storybook'),
+					}),
+				],
+				test: {
+					name: 'storybook',
+					browser: {
+						enabled: true,
+						headless: true,
+						provider: 'playwright',
+						instances: [
+							{
+								browser: 'chromium',
+							},
+						],
+					},
+					setupFiles: ['.storybook/vitest.setup.ts'],
+				},
+			},
+		],
 	},
 });
